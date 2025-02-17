0522ee11550af31629c8059495e34aabc6b3dd3f
Aliaksandr Artsymenia <143950309+Dooralove@users.noreply.github.com>
2025-02-18 00:46:50 +0300

    fix security warning
    fix 1 security warning

--- src/main/java/com/example/simpleblogapi/controllers/ArticleController.java
+++ src/main/java/com/example/simpleblogapi/controllers/ArticleController.java
@@ -1,20 +1,22 @@
 package com.example.simpleblogapi.controllers;
 
 import com.example.simpleblogapi.models.Article;
+import java.time.LocalDate;
+import java.security.SecureRandom;
+import java.util.Random;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.Random;
+
 
 @RestController
 public class ArticleController {
 
-    private static final Random random = new Random();
+    private static final SecureRandom random = new SecureRandom();
 
     @GetMapping("/articles")
-    public Article getArticle(@RequestParam(name = "Name", defaultValue = "TEST_NAME") String name) {
+    public Article getArticle(@RequestParam(name = "Name", defaultValue = "TEST_NAM") String name) {
         int randomLikes = random.nextInt(500);
         return new Article(name, LocalDate.now(), randomLikes);
     }
