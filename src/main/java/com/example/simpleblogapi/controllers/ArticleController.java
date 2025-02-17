586d2d8db0b98045fbf50e68bd4d0b5ff5ca169e
Aliaksandr Artsymenia <143950309+Dooralove@users.noreply.github.com>
2025-02-18 01:14:27 +0300

    Add random likes, dislikes, content, and comments to articles
    - Added fields for dislikes, article text and comments to the Article model.
    - Implemented generation of random values for likes, dislikes, content and comments in the controller.
    - The API now returns randomly generated data when requesting an article.

--- src/main/java/com/example/simpleblogapi/controllers/ArticleController.java
+++ src/main/java/com/example/simpleblogapi/controllers/ArticleController.java
@@ -1,13 +1,13 @@
 package com.example.simpleblogapi.controllers;
 
 import com.example.simpleblogapi.models.Article;
-import java.time.LocalDate;
 import java.security.SecureRandom;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 @RestController
 public class ArticleController {
@@ -16,10 +16,11 @@ public class ArticleController {
 
     @GetMapping("/articles")
     public Article getArticle(@RequestParam(name = "Name", defaultValue = "TEST_NAM") String name) {
-        int randomLikes = random.nextInt(500);
-        return new Article(name, LocalDate.now(), randomLikes);
+        int randLikes = random.nextInt(500);
+        int randomDislikes = random.nextInt(100);
+        String randomCont = "This is article))))))))))))))))))))))))))))))))))lol.";
+        List<String> randCom = Arrays.asList("Great!",  "Not bad.", "Could be better.");
+
+        return new Article(name, LocalDate.now(), randLikes, randomDislikes, randomCont, randCom);
     }
 }
-
-
-
--- src/main/java/com/example/simpleblogapi/models/Article.java
+++ src/main/java/com/example/simpleblogapi/models/Article.java
@@ -1,16 +1,23 @@
 package com.example.simpleblogapi.models;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Article {
     private String title;
     private LocalDate date;
     private int likes;
+    private int dislikes;
+    private String content;
+    private List<String> comments;
 
-    public Article(String title, LocalDate date, int likes) {
+    public Article(String title, LocalDate date, int likes, int dislikes, String content, List<String> comments) {
         this.title = title;
         this.date = date;
         this.likes = likes;
+        this.dislikes = dislikes;
+        this.content = content;
+        this.comments = comments;
     }
 
     public String getTitle() {
@@ -23,5 +30,17 @@ public class Article {
 
     public int getLikes() {
         return likes;
+    }
+
+    public int getDislikes() {
+        return dislikes;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public List<String> getComments() {
+        return comments;
     }
 }
